e59e806f3f5e33c8f228c443e93a81adae610ec6
erfan <erfano2013@gmail.com>
2024-04-02 10:16:01 +0330

    fix footer

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { categories } from "../data/data";
 const Category = () => {
   return (
-  <div className="max-w-[1640px] m-auto px-4 py-12 grid md:grid-cols-3 gap-6">
+  <div className="max-w-[1640px] m-auto px-4 py-12">
     <h1 className="text-orange-600 font-bold text-4xl text-center">Top Rated Menu Items</h1>
     <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
         {categories.map((item, index)=> (
-            <div key={index} className="bg-gray-100 rounded-lg =-4 flex justify-center items-center">
+            <div key={index} className="bg-gray-100 rounded-lg p=4 flex justify-center items-center">
                 <h2 className="font-bold sm:text-xl">{item.name}</h2>
                 <img className="w-20" src={item.image} alt={item.name} />
                 </div>
